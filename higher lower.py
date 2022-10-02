b5c3d1ad0794b5e7e9d232e605a51be1ea7abcf1
Otuagomah Samuel <50140919+Psalmi@users.noreply.github.com>
2022-10-02 19:39:55 +0100

    Update higher lower.py
    Your score is gets added.

--- higher lower.py
+++ higher lower.py
@@ -1,4 +1,6 @@
 import random
+correct_guess = 0
+wrong_guess = 0
 
 while True:
   com_num = random.randint(1, 50)
@@ -7,11 +9,14 @@ while True:
   while player_num != com_num:
     if player_num > com_num:
       print("Too high")
+      wrong_guess += 1
     else:
       print("Too low")
+      wrong_guess += 1
     player_num = int(input("enter a number between 1 - 50: "))
   else:
     print("Correct Guess, you're awesome!")
+    correct_guess += 1
 
   play_again = input("Play again? (yes/no): ")
 
@@ -19,3 +24,5 @@ while True:
     break
 
 print("Bye!")
+print(str(correct_guess) +" Correct guesses")
+print(str(wrong_guess) + " Wrong guesses")
